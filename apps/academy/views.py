014db48ca9fc7d09299f0d71f6eb521656dbeaaa
herrscherasd <turdalievasyl@gmail.com>
2023-05-03 23:40:07 +0600

    added contacts, about parts

--- apps/academy/views.py
+++ apps/academy/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.views import generic
+from django.urls import reverse_lazy
+
+from apps.academy.models import Academy
 
 # Create your views here.
+class IndexView(generic.ListView):
+    template_name = 'index.html'
+    queryset = Academy.objects.all()
+
+class AboutView(generic.TemplateView):
+    template_name = 'about.html'
+
+class ContactsView(generic.TemplateView):
+    template_name = 'contacts.html'
+
