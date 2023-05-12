f9a242f96899f7eb533197585f212b0fb835b0d7
Asylbek Turdaliev <turdalievasyl@gmail.com>
2023-05-12 13:48:12 +0600

    added

--- core/settings.py
+++ core/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_DIRS = [BASE_DIR / '/static/']
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR/ 'media'
