98750dea13738a8d9b243c182af0cb63564ec18d
Asylbek Turdaliev <turdalievasyl@gmail.com>
2023-05-12 13:40:54 +0600

    added

--- core/settings.py
+++ core/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "z6#tbjx3uk8=y4&x6ddw5yv2239l=(&+pwk$zq1crcwdky@rr5"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
