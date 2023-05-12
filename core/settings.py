a069dda04c7000f7be2c1ac3160645f6d03636ee
Asylbek Turdaliev <turdalievasyl@gmail.com>
2023-05-12 15:47:00 +0600

    added

--- core/settings.py
+++ core/settings.py
@@ -119,7 +119,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 MEDIA_URL = '/media/'
